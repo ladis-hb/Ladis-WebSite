8a20502770adcf32b534a3b61daecc683c394ab4
wgcairui <wgcairui@icloud.com>
2019-10-17 09:31:51 +0800

    add

--- pages/support/index.vue
+++ pages/support/index.vue
@@ -1,37 +1,37 @@
 <template>
   <div class="goBug">
     <div class="mall">
-      <a class="external" href="/support/监控软件下载" target="_blank">
+      <a class="external" href="/support/监控软件下载">
         <img src="/_CMS_NEWS_IMG_/2016-12/28/4085c9fe-3f3c-47e9-9349-cca148fe2615.jpg" alt />
       </a>
 
-      <a class="noMargin external" href="/support/产品彩页说明" target="_blank">
+      <a class="noMargin external" href="/support/产品彩页说明" >
         <img src="/_CMS_NEWS_IMG_/2016-12/28/d8e7327f-4105-488c-9d82-79d8215f5b5c.jpg" alt />
       </a>
     </div>
 
     <div class="mall">
-      <a class="external" href="/support/证书资质" target="_blank">
+      <a class="external" href="/support/证书资质">
         <img src="/_CMS_NEWS_IMG_/2016-12/28/5c29cbe2-73f1-4d2e-8924-95217b17b2a4.jpg" alt />
       </a>
 
-      <a class="noMargin external" href="/support/维修服务" target="_blank">
+      <a class="noMargin external" href="/support/维修服务" >
         <img src="/_CMS_NEWS_IMG_/2016-12/28/9a031ec6-5ec8-477a-b2a4-ac4edfeead97.jpg" alt />
       </a>
     </div>
 
     <div class="mall">
-      <a class="external" href="/support/常见问题" target="_blank">
+      <a class="external" href="/support/常见问题" >
         <img src="/_CMS_NEWS_IMG_/2017-08/22/4c20bc9f-3046-48ce-bfad-be79dab65579.jpg" alt />
       </a>
 
-      <a class="noMargin external" href="/about/联系我们" target="_blank">
+      <a class="noMargin external" href="/about/联系我们" >
         <img src="/_CMS_NEWS_IMG_/2017-08/22/a22c716a-2344-45f4-be62-75c962b3e0a6.jpg" alt />
       </a>
     </div>
 
     <div class="mall">
-      <a class="external" href="http://www.pesiv.com" target="_blank">
+      <a class="external" href="http://www.pesiv.com" >
         <img src="/_CMS_NEWS_IMG_/2017-08/22/121050d6-932a-44fc-9527-08934db4f427.jpg" alt />
       </a>
     </div>
