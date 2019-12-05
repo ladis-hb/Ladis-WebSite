1448feaac4e9c6462a34d74bfe4a12e21b692c60
wgcairui <wgcairui@icloud.com>
2019-12-05 15:52:36 +0800

    add

--- server/mongoose/content.js
+++ server/mongoose/content.js
@@ -156,7 +156,7 @@ const Schema_CaseNews_list = new Schema(
 const Schema_About = new Schema({
   title: String,
   webSite:String,
-  body: [String]
+  body: String
 });
 const Schema_Head = new Schema(
   {
