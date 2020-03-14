0b47a1387f04f86a9b07996641a5daa89ec845a5
wgcairui <wgcairui@icloud.com>
2020-03-14 11:46:41 +0800

    add

--- api/axios.js
+++ api/axios.js
@@ -1,11 +1,12 @@
 /* jshint esversion:8 */
 import axios from "axios";
-
+import config from "../nuxt.config";
 async function ajax({ method = "get", url, baseURL, params }) {
   let { data } = await axios({
     method: method,
     url: url,
-    baseURL: baseURL || "",
+    baseURL: baseURL || "http://www.ladishb.com:" + config.server.port,
+
     params: Object.assign(getToken(), params || {})
   });
   return data;
@@ -43,10 +44,10 @@ export const setCarousel = params => {
   return ajax({ url: "/edit/setCarousel", params });
 };
 
-export const setAbout = params =>{
-  return ajax({url:"/edit/setAbout",params})
-}
+export const setAbout = params => {
+  return ajax({ url: "/edit/setAbout", params });
+};
 
-export const getAbout = params =>{
-  return ajax({url:"/edit/getAbout",params})
-}
+export const getAbout = params => {
+  return ajax({ url: "/edit/getAbout", params });
+};
